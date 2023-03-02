8c4aa6c0746bd91f4801dfeeb490707e911eddfa
ts0yu <ts0yu@proton.me>
2023-03-02 20:04:02 +0000

    latest

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -1,3 +1,92 @@
+use clap::{Parser, Subcommand};
+use colored::*;
+
+use compiler::{
+	assembler::{Assembler, Macro},
+	token::{Token, TokenType}
+};
+
+use std::{
+	fs, 
+	time::Instant,
+	collections::HashMap
+};
+
+#[derive(Parser)]
+#[clap(about, version, author)]
+struct Value {
+    #[clap(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Run { path: String },
+}
+
 fn main() {
-    println!("Hello, world!");
+    let value = Value::parse();
+
+    match &value.command {
+        Commands::Run { path } => {
+            let contents = fs::read_to_string(path).unwrap();
+            let tokens = Token::lex(&contents);
+            let mut main_macro: Macro = Macro {
+                name: "",
+                body: Vec::new(),
+            };
+
+            let mut occurences: usize = 0;
+
+            tokens.clone().into_iter().for_each(|token| {
+                if token.ttype == TokenType::Macro {
+                    occurences += 1
+                }
+            });
+
+            let opcodes = Assembler::new(tokens);
+            let mut macros = HashMap::new();
+
+            for _ in 0..occurences {
+                let mac = opcodes.parse_macro().unwrap();
+
+                if macros.get(&mac.name).is_some() {
+                    println!(
+                        "{}: macro with name `{}` already exists",
+                        "error".red().bold(),
+                        &mac.name
+                    );
+                    std::process::exit(1);
+                }
+
+                if macros.get(&mac.name).is_none() {
+                    macros.insert(mac.clone().name, mac);
+                }
+            }
+
+            match macros.get(&"main") {
+                Some(r#main) => main_macro = r#main.clone(),
+                None => panic!("no main macro found"),
+            }
+
+            for (i, n) in main_macro.clone().body.iter().enumerate() {
+                if n.ttype == TokenType::Identifier {
+                    let replacer = macros.get(&n.slice);
+                    let mut index: usize = i;
+
+                    for g in &replacer.unwrap().body {
+                        main_macro.body.insert(index, *g);
+                        index += 1;
+                    }
+                }
+            }
+
+            println!("{} `{}`", "Compiling".green().bold(), path);
+
+            let now = Instant::now();
+            let opcodes = Assembler::new(main_macro.body).assemble();
+
+            println!("Opcodes: {opcodes:#?}");
+        }
+    }
 }
